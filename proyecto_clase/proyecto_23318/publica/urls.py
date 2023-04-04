35b1dad34297ab49eea0c9e5263baa5c2b555e0a
Fede Liquin <josefliquin@gmail.com>
2023-04-04 19:37:05 -0300

    Reunion 11: actualizacion de avances del proyecto

--- proyecto_clase/proyecto_23318/publica/urls.py
+++ proyecto_clase/proyecto_23318/publica/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [    
     path('', views.index, name='inicio'),
-    path('quienes_somos', views.quienes_somos, name='quienes_somos'),
-    path('saludar/<str:nombre>',views.saludar)
+    path('quienes_somos/', views.quienes_somos, name='quienes_somos'),
+    path('saludar/<str:nombre>/',views.saludar),
+    
+    path('proyectos/<int:anio>/<int:mes>/',views.ver_proyectos,name='ver_proyectos'),
+    path('proyectos/2023/04/',views.ver_proyectos_2023_04,name='ver_proyectos_23_04'),
+    path('proyectos/<int:anio>/',views.ver_proyectos,name='ver_proyectos_defecto'),
+
 ]
--- proyecto_clase/proyecto_23318/publica/views.py
+++ proyecto_clase/proyecto_23318/publica/views.py
@@ -4,7 +4,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse(f"""<h1>Proyecto Django - Codo a Codo🦄</h1>""")
+    if(request.GET.get('param')):
+        param_uno = request.GET.get('param')
+    else:
+        param_uno='defecto'
+    param_dos = request.GET.get('param2')
+    return render(request,'publica/index.html',{'param_uno':param_uno})
+    # return HttpResponse(f"""
+    #     <h1>Proyecto Django - Codo a Codo🦄</h1>
+    #     <p>Param: {param_uno}</p>
+    #     <p>Param 2: {param_dos}</p>
+    #     """)
     
 def quienes_somos(request):
     return HttpResponse(f"""<h1>Quienes somos 🙇‍♂️</h1>""")
@@ -17,4 +27,14 @@ def saludar(request,nombre):
     else:
         return HttpResponse(f"""
             <h1>Hola Django - {nombre}</h1>
+        """)
+    
+def ver_proyectos(request,anio,mes=1):    
+    return HttpResponse(f"""
+            <h1>Proyectos del  {mes}/{anio}</h1>
+        """)
+
+def ver_proyectos_2023_04(request):    
+    return HttpResponse(f"""
+            <h1>Proyectos del mes de Abril 2023</h1>
         """)
