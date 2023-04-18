192dcdc7babee00b53dad090474c365ed634b2f4
Fede Liquin <josefliquin@gmail.com>
2023-04-18 19:59:20 -0300

    Se actualiza codigo del proyecto que se verá en la reunion 16 sobre formularios

--- proyecto_clase/proyecto_23318/publica/urls.py
+++ proyecto_clase/proyecto_23318/publica/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [    
     path('', views.index, name='inicio'),
-    path('quienes_somos/', views.quienes_somos, name='quienes_somos'),
+    path('quienes_somos/',views.quienes_somos,name='quienes_somos'),
+    path('proyectos/',views.proyectos,name="proyectos"),
+    path('api_proyectos/',views.api_proyectos,name="api_proyectos"),
+    path('cursos/',views.ver_cursos,name="cursos"),
+
     path('saludar/<str:nombre>/',views.saludar),
-    
     path('proyectos/<int:anio>/<int:mes>/',views.ver_proyectos,name='ver_proyectos'),
     path('proyectos/2023/04/',views.ver_proyectos_2023_04,name='ver_proyectos_23_04'),
     path('proyectos/<int:anio>/',views.ver_proyectos,name='ver_proyectos_defecto'),
--- proyecto_clase/proyecto_23318/publica/views.py
+++ proyecto_clase/proyecto_23318/publica/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
+
+from publica.forms import ContactoForm
 
 from datetime import datetime
 
 # Create your views here.
-def index(request):
+def index(request):    
+    mensaje=None
+    if(request.method=='POST'):
+        contacto_form = ContactoForm(request.POST)
+        mensaje='Hemos recibido tus datos'
+        # acción para tomar los datos del formulario
+    else:
+        contacto_form = ContactoForm()
     listado_cursos = [
         {
             'nombre':'Fullstack Java',
@@ -23,16 +32,71 @@ def index(request):
             'descripcion':'test',
             'categoria':'Análisis de Datos',
         },
+        {
+            'nombre':'Big Data Avanzado',
+            'descripcion':'test',
+            'categoria':'Análisis de Datos',
+        },
     ]
-    return render(request,'publica/index.html',{
-                    'cursos':listado_cursos,})
-    
+
+    context = {                
+                'cursos':listado_cursos,
+                'mensaje':mensaje,
+                'contacto_form':contacto_form
+            }
+    return render(request,'publica/index.html',context)
+
 def quienes_somos(request):
     template = loader.get_template('publica/quienes_somos.html')
     context = {'titulo':'Codo A Codo - Quienes Somos'}
     return HttpResponse(template.render(context,request))
 
-    
+def ver_cursos(request):
+    listado_cursos = [
+        {
+            'nombre':'Fullstack Java',
+            'descripcion':'Curso de Fullstack',
+            'categoria':'Programación',             
+        },
+        {
+            'nombre':'Diseño UX/UI',
+            'descripcion':'🖌🎨',
+            'categoria':'Diseño',
+        },
+        {
+            'nombre':'Big Data',
+            'descripcion':'test',
+            'categoria':'Análisis de Datos',
+        },
+        {
+            'nombre':'Big Data Avanzado',
+            'descripcion':'test',
+            'categoria':'Análisis de Datos',
+        },
+    ]
+    return render(request,'publica/cursos.html',{'cursos':listado_cursos})
+
+def api_proyectos(request,):
+    proyectos = [{
+        'autor': 'Gustavo Villegas',
+        'portada': 'https://agenciadeaprendizaje.bue.edu.ar/wp-content/uploads/2021/12/Gustavo-Martin-Villegas-300x170.png',
+        'url':'https://marvi-artarg.web.app/'
+    },{
+        'autor': 'Enzo Martín Zotti',
+        'portada': 'https://agenciadeaprendizaje.bue.edu.ar/wp-content/uploads/2022/01/Enzo-Martin-Zotti-300x170.jpg',
+        'url':'https://hablaconmigo.com.ar/'
+    },{
+        'autor': 'María Echevarría',
+        'portada': 'https://agenciadeaprendizaje.bue.edu.ar/wp-content/uploads/2022/01/Maria-Echevarria-300x170.jpg',
+        'url':'https://compassionate-colden-089e8a.netlify.app/'
+    },]
+    response = {'status':'Ok','code':200,'message':'Listado de proyectos','data':proyectos}
+    return JsonResponse(response,safe=False)
+
+def proyectos(request):    
+    return render(request,'publica/proyectos.html')
+
+#NO SE USAN    
 def saludar(request,nombre):
     if(request.method=='GET'):
         return HttpResponse(f"""
