016e6c46a5b374ac8171ed0b31ebaa34813885a6
Fede Liquin <josefliquin@gmail.com>
2023-05-09 20:03:37 -0300

    REUNION 22: Ejemplos de modelos para analizar

--- proyecto_clase/proyecto_23318/administracion/models.py
+++ proyecto_clase/proyecto_23318/administracion/models.py
@@ -56,8 +56,7 @@ from django.db import models
 
 # Create your models here.
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=50,verbose_name='Nombre')
-    descripcion = models.TextField(null=True,verbose_name='Descripcion')
+    nombre = models.CharField(max_length=50,verbose_name='Nombre')    
     baja = models.BooleanField(default=0)
 
     def __str__(self):
